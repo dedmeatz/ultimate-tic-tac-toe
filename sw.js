12c4601f3ce505cec2c80ce605b034d010d60574
dedmeatz <dumbledores.aide@gmail.com>
2026-08-31 11:06:15 +0800

    initial commit for pwa

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ulti-tic-tac-toe';
+const ASSETS = [
+    './',
+    './index.html',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event (Standard Cache-First Strategy)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
